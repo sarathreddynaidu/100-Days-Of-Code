afba115ceb46e0bbfb5e271002fa446f005ba528
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2021-04-12 15:40:37 -0400

    Update event_listener.py

--- Day19_Event-Listners_Higher-Order-Functions/event_listener.py
+++ Day19_Event-Listners_Higher-Order-Functions/event_listener.py
@@ -10,6 +10,26 @@ def move_forward():
     tim.forward(10)
 
 
-screen.onkey(key="space", fun=move_forward)
+def move_backward():
+    tim.backward(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def clear_screen():
+    tim.reset()
+
+
+screen.onkey(fun=move_forward, key="w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear_screen, "c")
 
 screen.exitonclick()
