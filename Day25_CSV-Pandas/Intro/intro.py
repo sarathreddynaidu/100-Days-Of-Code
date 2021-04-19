2555517d5d8cf277caa59c95e64d1092971d3ff4
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2021-04-19 12:22:24 -0400

    Create intro.py

---
+++ Day25_CSV-Pandas/Intro/intro.py
@@ -0,0 +1,19 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temp = int(row[1])
+#             temperatures.append(temp)
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
