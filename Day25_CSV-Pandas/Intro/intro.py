94e3a6584c183881062da41359e232e9d95b05f0
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2021-04-19 14:26:12 -0400

    Update intro.py

--- Day25_CSV-Pandas/Intro/intro.py
+++ Day25_CSV-Pandas/Intro/intro.py
@@ -16,4 +16,29 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+print(data)
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+print(data["temp"].mean())
+print(data["temp"].max())
+
+# get data in columns
+print(data["condition"]) # or print(data.condition) # these are case sensitive
+
+# get data in row
+print(data[data.day == "Monday"]) # day is monday
+print(data[data.temp == data.temp.max()]) # max temperature
+
+# create data from scratch
+data_Dict1 = {
+    "students": ["Sai", "pru", "Mah", "Mru"],
+    "scores": [99, 97, 95, 92]
+}
+data1 = pandas.DataFrame(data_Dict1)
+print(data1)
+data1.to_csv("new_data.csv") # creates new csv file
