9275638e01b3e9f3e885bc51d656b5350367e5fc
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2021-04-22 12:50:09 -0400

    Update dict_comp.py

--- Day26_List-Dict-Comprehension/dict_comp.py
+++ Day26_List-Dict-Comprehension/dict_comp.py
@@ -1,3 +1,5 @@
+# 1
+
 import random
 
 names = ["sai", "prudhvi", "mahesh", "mrudula"]
@@ -7,3 +9,23 @@ print(scores)
 
 passed = {name: score for (name, score) in scores.items() if score > 60}
 print(passed)
+
+# 2 
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+result = {word: len(word) for word in sentence.split()}
+print(result)
+
+# 3
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+weather_f = {day: (temp*9/5)+32 for (day, temp) in weather_c.items()}
+print(weather_f)
