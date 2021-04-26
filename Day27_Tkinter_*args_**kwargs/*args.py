47b6799f86ad15340ec4bf27aff676678d48b3be
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2021-04-26 18:03:24 -0400

    Create *args.py

---
+++ Day27_Tkinter_*args_**kwargs/*args.py
@@ -0,0 +1,16 @@
+def my_func(*args):
+    for n in args:
+        print(n)
+
+my_func(1, 2, 3)
+my_func(10, 45)
+
+# add
+def add(*args):
+    sol = 0
+    for n in args:
+        sol += n
+    print(sol)
+
+add(10, 45)
+add(4, 5)
